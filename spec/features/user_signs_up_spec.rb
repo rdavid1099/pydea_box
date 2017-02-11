64d50574e64277752128e7c5f162c12382100e99
Ryan Workman <rdavid1099@gmail.com>
2017-02-11 15:44:39 -0700

    Sad paths all fully tested

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -20,4 +20,58 @@ describe 'user creates an account' do
       expect(page).to have_content('Total Ideas: 0')
     end
   end
+
+  context 'not all fields filled in' do
+    it 'forgets username and is kicked back' do
+      visit new_user_path
+      fill_in 'user_email', with: 'test@testing.io'
+      fill_in 'user_password', with: 'testingpassword'
+      fill_in 'user_password_confirmation', with: 'testingpassword'
+      click_on 'Create Account'
+
+      expect(page).to have_content("Username can't be blank")
+    end
+
+    it 'forgets email and is kicked back' do
+      visit new_user_path
+      fill_in 'user_username', with: 'testing'
+      fill_in 'user_password', with: 'testingpassword'
+      fill_in 'user_password_confirmation', with: 'testingpassword'
+      click_on 'Create Account'
+
+      expect(page).to have_content("Email can't be blank")
+    end
+
+    it 'forgets password and is kicked back' do
+      visit new_user_path
+      fill_in 'user_username', with: 'testing'
+      fill_in 'user_email', with: 'test@testing.io'
+      fill_in 'user_password_confirmation', with: 'testingpassword'
+      click_on 'Create Account'
+
+      expect(page).to have_content("Password can't be blank")
+    end
+
+    it 'forgets password confirmation and is kicked back' do
+      visit new_user_path
+      fill_in 'user_username', with: 'testing'
+      fill_in 'user_email', with: 'test@testing.io'
+      fill_in 'user_password', with: 'testingpassword'
+      click_on 'Create Account'
+
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
+
+    it 'does not have a unique email address' do
+      create_user
+      visit new_user_path
+      fill_in 'user_username', with: 'testing'
+      fill_in 'user_email', with: 'test@tester.com'
+      fill_in 'user_password', with: 'testingpassword'
+      fill_in 'user_password_confirmation', with: 'testingpassword'
+      click_on 'Create Account'
+
+      expect(page).to have_content("Email has already been taken")
+    end
+  end
 end
